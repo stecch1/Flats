e743ff72e6891e173227a5d83b096c92dee8a128
AlessandroPolidori <10573078@polimi.it>
2021-10-18 19:41:36 +0200

    this is a test

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart'; 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//test commit ale
 
 void main() {
   runApp(const MyApp());
