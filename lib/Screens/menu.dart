ed990684a013b459f198e8d902cd62179de2f962
AlessandroPolidori <10573078@polimi.it>
2021-10-26 13:03:09 +0200

    added logout in menu

--- lib/Screens/menu.dart
+++ lib/Screens/menu.dart
@@ -1,4 +1,6 @@
+import 'package:flats/Services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Menu extends StatelessWidget {
   const Menu({Key? key}) : super(key: key);
@@ -6,9 +8,17 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final authService = Provider.of<AuthService>(context);
       return Scaffold(
         appBar: AppBar(
           toolbarHeight: 38,
+          automaticallyImplyLeading: false,
+          leading: IconButton (
+            icon: Icon(Icons.logout, semanticLabel: "sign out",),
+            onPressed: () {
+              authService.signOut();
+            },
+          ),
         ),
         body: const Center(
           child: Text(
