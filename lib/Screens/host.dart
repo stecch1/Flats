4c45e6941c6d52b362314760bade22ecceb62d7f
AlessandroPolidori <10573078@polimi.it>
2021-10-26 18:07:23 +0200

    login system finished

--- lib/Screens/host.dart
+++ lib/Screens/host.dart
@@ -1,4 +1,8 @@
+import 'package:flats/Models/user_model.dart';
+import 'package:flats/Screens/login_screen.dart';
+import 'package:flats/Services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HostScreen extends StatefulWidget {
   const HostScreen({Key? key}) : super(key: key);
@@ -7,11 +11,22 @@ class HostScreen extends StatefulWidget {
   _HostScreenState createState() => _HostScreenState();
 }
 
+
 class _HostScreenState extends State<HostScreen> {
+  String _email = "";
+
   @override
   Widget build(BuildContext context) {
+
+    final authService = Provider.of<AuthService>(context);
+    authService.user!.listen(
+          (data) =>   _email = data!.email!,
+    );
     return Container(
-      child: Text("Host"),
+      child: FlatButton(
+        child: Text("print email on console"),
+        onPressed: () {print(_email);},
+      ),
     );
   }
 }
