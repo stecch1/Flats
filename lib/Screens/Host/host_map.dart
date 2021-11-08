54deff516a19cabe229d1d65ecfe671bcb214a61
AlessandroPolidori <10573078@polimi.it>
2021-11-08 17:47:36 +0100

    host section version 1.0 done

--- lib/Screens/Host/host_map.dart
+++ lib/Screens/Host/host_map.dart
@@ -4,13 +4,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flats/Services/auth_service.dart';
 import 'package:flats/Utils/image_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as pt;
 import 'package:provider/provider.dart';
-
 
 class HostMap extends StatefulWidget {
   HostMap({Key? key, required String this.uid}) : super(key: key);
@@ -21,27 +21,23 @@ class HostMap extends StatefulWidget {
   _HostMapState createState() => _HostMapState();
 }
 
-
-
 class _HostMapState extends State<HostMap> {
-
   Set<Marker> markers = Set();
-  TextEditingController controller1 = TextEditingController ();
-  TextEditingController controller2 = TextEditingController ();
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
   File? img;
-
 
   @override
   Widget build(BuildContext context) {
-
     final authService = Provider.of<AuthService>(context);
-
 
     return SafeArea(
       child: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("Location").where('uid', isEqualTo: widget.uid).snapshots(),
+        stream: FirebaseFirestore.instance
+            .collection("Location")
+            .where('uid', isEqualTo: widget.uid)
+            .snapshots(),
         builder: (context, AsyncSnapshot snapshot) {
-
           if (!snapshot.hasData) {
             return Scaffold(
               body: Container(
@@ -57,7 +53,7 @@ class _HostMapState extends State<HostMap> {
               location = document['location'];
               var latLng = LatLng(location.latitude, location.longitude);
               markers.add(Marker(
-                  markerId: MarkerId(document.id),
+                markerId: MarkerId(document.id),
                 position: latLng,
                 infoWindow: InfoWindow(
                   title: document['name'],
@@ -68,21 +64,19 @@ class _HostMapState extends State<HostMap> {
             } else {
               print('document does not exist');
             }
-          }
-
-          );
+          });
 
           return Scaffold(
             body: GoogleMap(
               myLocationButtonEnabled: false,
               zoomControlsEnabled: false,
               initialCameraPosition: const CameraPosition(
-                target: LatLng(45.478436, 9.226619), zoom: 11.5,),
+                target: LatLng(45.478436, 9.226619),
+                zoom: 11.5,
+              ),
               markers: markers,
-
             ),
           );
-
         },
       ),
     );
@@ -93,129 +87,178 @@ class _HostMapState extends State<HostMap> {
     await showModalBottomSheet(
         context: context,
         builder: (BuildContext context) {
-
           return StatefulBuilder(
-            builder: (BuildContext context, StateSetter mystate){
-              String src = "";
+              builder: (BuildContext context, StateSetter mystate) {
+            String src = "";
 
-            return Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      MaterialButton(
-                        onPressed: () {
-                          FirebaseFirestore.instance
-                              .collection("Location")
-                              .doc(document.id)
-                              .delete();
-                          markers.removeWhere((element) => element.markerId.value == document.id);
-                        },
-                        color: Colors.red,
-                        textColor: Colors.white,
-                        child: const Text("Delete"),
+            return Container(
+              margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          MaterialButton(
+                            onPressed: () {
+                              FirebaseFirestore.instance
+                                  .collection("Location")
+                                  .doc(document.id)
+                                  .delete();
+                              markers.removeWhere((element) =>
+                                  element.markerId.value == document.id);
+                            },
+                            color: Colors.red,
+                            textColor: Colors.white,
+                            child: const Text("Delete"),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                //TODO: add margin to these rows below
-                Row(
-                  children: [Text("flat's name: " + document['name'])],
-                ),
-                Row(
-                  children: [Text("euro/month: " + document['price'].toString())],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                        child: Text("description: " + document['description']))
-                  ],
-                ),
-                map.containsKey("urls") == true ?
-                    ImageView(map: map,)
+                    ),
+                    //TODO: add margin to these rows below
 
-                    : Container(),
-
-                Column(
-                  children: [
                     Row(
                       children: [
-                        ElevatedButton(
-                            onPressed: () {
-                              _pickImage().then((value) => mystate(() {}));
-                            },
-                            child: const Text("+ add photo")),
-
-                        ElevatedButton(
-                            onPressed: () {
-                              _uploadImage(document).then((value) => {
-                                    setState(() {
-                                      mystate(() {
-                                        Navigator.pop(context);
-                                        Navigator.pop(context);
-                                      });
-                                    })
-                                  });
-                            },
-                            child: const Text("Upload photo")),
+                        const Text(
+                          "flat's name: ",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          document['name'],
+                          style: const TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
                       ],
                     ),
-                    Text(img != null ? img.toString() : ""),
+
+                    Row(
+                      children: [
+                        const Text(
+                          "euro/month: ",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          document['price'].toString(),
+                          style: const TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "description: ",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            document['description'],
+                            style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    map.containsKey("urls") == true
+                        ? ImageView(
+                            map: map,
+                          )
+                        : Container(),
+
+                    Column(
+                      children: [
+                        Row(
+
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {
+                                  _pickImage().then((value) => mystate(() {}));
+                                },
+                                child: const Text("+ add photo")),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(6, 0, 0, 0),
+                              child: ElevatedButton(
+
+                                  onPressed: () {
+                                    _uploadImage(document).then((value) => {
+                                          setState(() {
+                                            mystate(() {
+                                              Navigator.pop(context);
+                                              Navigator.pop(context);
+                                            });
+                                          })
+                                        });
+                                  },
+                                  child: const Text("Upload photo")),
+                            ),
+                          ],
+                        ),
+                        Text(img != null ? img.toString() : ""),
+                      ],
+                    ),
                   ],
                 ),
 
-
-              ],
             );
           });
         }).whenComplete(() => img = null);
     setState(() {});
   }
 
-  Future _pickImage() async{
+  Future _pickImage() async {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
 
-      if(image == null) return;
-      final imagePath =  File(image.path);
+      if (image == null) return;
+      final imagePath = File(image.path);
 
       this.img = imagePath;
-
-
     } on PlatformException catch (e) {
       print("Failed to pick image $e");
     }
-
   }
 
-  Future _uploadImage(dynamic document) async{
+  Future _uploadImage(dynamic document) async {
     if (img == null) return;
     String id = document.id.toString();
     String name = pt.basename(img!.path);
-    final destination = 'images/'+id+"/"+name;
+    final destination = 'images/' + id + "/" + name;
     firebase_storage.FirebaseStorage storage =
         firebase_storage.FirebaseStorage.instance;
 
     try {
-      await storage
+      await storage.ref(destination).putFile(img!).whenComplete(() => storage
           .ref(destination)
-          .putFile(img!).whenComplete(() => storage.ref(destination).getDownloadURL().then((value) =>
-          FirebaseFirestore.instance.collection('Location').doc(document.id).update(
-              {
-                "urls" : FieldValue.arrayUnion([value]),
-              }
-          )
-
-      ));
-
+          .getDownloadURL()
+          .then((value) => FirebaseFirestore.instance
+                  .collection('Location')
+                  .doc(document.id)
+                  .update({
+                "urls": FieldValue.arrayUnion([value]),
+              })));
     } on firebase_storage.FirebaseException catch (e) {
       print(e);
     }
-
-
   }
-
-
 }
--- lib/Utils/image_view.dart
+++ lib/Utils/image_view.dart
@@ -3,12 +3,36 @@ import 'package:flutter/cupertino.dart';
 class ImageView extends StatelessWidget {
   ImageView({Key? key, required this.map}) : super(key: key);
   Map<String, dynamic> map;
+
   @override
   Widget build(BuildContext context) {
-     return map["urls"].length == 0 ? Container()
-         : Image.network(map["urls"][0],
-            width: 400,
-            height: 140,) ;
+
+
+
+    return map["urls"].length == 0
+        ? Container()
+        : SizedBox(
+            height: 140.0,
+            width: double.infinity,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: map["urls"].length,
+              itemBuilder: (BuildContext context, int index) => Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Image.network(
+                  map["urls"][index],
+                  width: 140,
+                  height: 140,
+                ),
+              ),
+            ),
+          );
+    /*
+     return Image.network(map["urls"][index],
+                          width: 400,
+                          height: 140,);
+     */
+
   }
-  //TODO: SUPPORT MORE THAN 1 pic VIEW
+
 }
