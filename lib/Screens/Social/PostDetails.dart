c8fb3916ae46d1afa9fbbe7c2830c770d60e4ab9
stecch1 <fabio.stecchi@mail.polimi.it>
2021-11-18 13:51:52 +0100

    starting point for Social

---
+++ lib/Screens/Social/PostDetails.dart
@@ -0,0 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+
+
+class PostDetails extends StatefulWidget {
+  Map<String, dynamic> data;
+  PostDetails(this.data,{ Key? key }) : super(key: key);
+
+
+  @override
+  _PostDetailsState createState() => _PostDetailsState();
+}
+
+class _PostDetailsState extends State<PostDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      appBar: new AppBar(
+        title: new Text("Post Details"),
+        backgroundColor: Colors.amber
+      ),
+      
+      body: new Card(
+        elevation: 10.0,
+        margin: EdgeInsets.all(10.0),
+        child: new ListView(
+          children:<Widget>[
+            new Container(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+
+                  new CircleAvatar(
+                     child: new Text(
+                      widget.data['title'][0]),
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.black
+                  ),
+
+                  new SizedBox(width: 10.0,),
+
+                  new Text(widget.data['title'],
+                    style: TextStyle(fontSize: 22.0, )
+                  ),
+
+                  new SizedBox(height: 7.0,),
+
+                  new Container(
+                    margin:EdgeInsets.all(1.0),
+                    child: Text(widget.data['content'],
+                      style: TextStyle(fontSize: 18.0, ),
+                      textAlign: TextAlign.justify,
+                      overflow: TextOverflow.ellipsis,
+                      )
+                  ),
+
+
+
+
+
+
+                ],
+              ),
+            )
+          ],
+        ),
+        
+      ),
+
+    );
+  }
+}
--- lib/Screens/Social/social.dart
+++ lib/Screens/Social/social.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path/path.dart';
+import 'PostDetails.dart';
+
 
 class SocialScreen extends StatefulWidget {
   const SocialScreen({Key? key}) : super(key: key);
@@ -8,8 +14,106 @@ class SocialScreen extends StatefulWidget {
 }
 
 class _SocialScreenState extends State<SocialScreen> {
+  
+  final Stream<QuerySnapshot> _postStream = FirebaseFirestore.instance.collection('Post').snapshots();
+
+
+  passData(Map<String, dynamic> data){
+    Navigator.of(this.context).push(new MaterialPageRoute(builder: (context)=>PostDetails(data)));
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder<QuerySnapshot>(
+      stream: _postStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        
+    return Container(
+       child: new ListView(
+         children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+           return new Card(
+             shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.black, width: 1),
+              borderRadius: BorderRadius.circular(5),
+             ),
+             elevation: 0.0,
+             margin: EdgeInsets.all(10.0),
+
+             child: new Container(
+               padding: EdgeInsets.all(10.0),
+               child: new Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children:<Widget> [
+                   new CircleAvatar(
+                     child: new Text(
+                      data['title'][0]),
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.black
+                  ),
+                   new Container(
+                     width: 210.0,
+                     child: new Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children:<Widget> [
+                         new InkWell(
+                           child: new Text(
+                             data['title'],
+                             style: const TextStyle(
+                               fontSize: 22.0,
+                               color: Colors.black,
+                             ),
+                             maxLines: 1,
+                             
+                           ),
+                           onTap: (){
+                             passData(data);
+                           }
+                         ),
+                         new SizedBox(height: 5.0,),
+
+                         new Text(
+                           data['content'],
+                           style: TextStyle(fontSize: 14),
+                           maxLines: 2,
+                         )
+                       ],
+                     ),
+
+                   )
+                 ],
+               ),
+             )
+           );
+    
+      }).toList(),
+    )
+    );
+      },
+    );
   }
 }
+
+
+/*return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['full_name']),
+              subtitle: Text(data['company']),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}*/
